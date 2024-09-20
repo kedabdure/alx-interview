35f23983ca9564bb6dea7b54147a15969565c54b
kedabudre <abdurehimk77@gmail.com>
2024-09-20 18:28:29 +0200

    update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/people/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-console.log(url);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body);
+    const characters = films.characters;
+
+    characters.forEach(character => {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.error(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      })
+    });
+  }
+});
