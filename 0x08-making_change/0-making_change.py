d4c4e8f1511dfc70e7383017e604cdb45fe1d9f1
kedabudre <abdurehimk77@gmail.com>
2024-10-04 04:55:51 +0200

    initial commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    return min_coins[total] if min_coins[total] != float('inf') else -1
